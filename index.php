7824bc9eeafeef3f814354fdc8237b2129dd2725
pupil <pupil@natella>
2015-09-23 16:32:29 +0300

    esimene komment

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>MYSite</title>
+</head>
+<body>
+	<h1>Список всех записей в таблице</h1>
+	<ol>
+		<li><?php echo "siia paneme kiri"; ?></li>
+		<li><?php echo "siia paneme kiri2"; ?></li>
+	</ol>
+
+</body>
+</html>
