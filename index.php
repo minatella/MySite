f2874656439af62cedf796d9d4cf4b913d1e43c1
pupil <pupil@natella>
2015-09-28 16:03:50 +0300

    teine komment

--- index.php
+++ index.php
@@ -2,14 +2,23 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>MYSite</title>
+	<title>Veebileht (front controller)</title>
 </head>
 <body>
 	<h1>Список всех записей в таблице</h1>
 	<ol>
-		<li><?php echo "siia paneme kiri"; ?></li>
+		<li><?php echo "siia paneme kiri1"; ?></li>
 		<li><?php echo "siia paneme kiri2"; ?></li>
+		<li><?php echo "siia paneme kiri3"; ?></li>
+		<li><?php echo "siia paneme kiri4"; ?></li>
 	</ol>
-
+	<h1>Список всех записей в таблице2</h1>
+	<ol>
+		<li><?php echo "siia paneme kiri1"; ?></li>
+		<li><?php echo "siia paneme kiri2"; ?></li>
+		<li><?php echo "siia paneme kiri3"; ?></li>
+		<li><?php echo "siia paneme kiri4"; ?></li>
+	</ol>
+<h2></h2>>
 </body>
 </html>
